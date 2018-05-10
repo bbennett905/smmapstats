7e65c5abb8223f064046d03855e7912a6ef3a243
Unknown <bbennett905@gmail.com>
2018-05-09 22:35:34 -0400

    Move main page to home

--- web/MapStatsWeb/Controllers/HomeController.cs
+++ web/MapStatsWeb/Controllers/HomeController.cs
@@ -1,23 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MapStatsWeb.Models;
 using Microsoft.AspNetCore.Mvc;
-using MapStatsWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace MapStatsWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly MapstatsContext _context;
+        public HomeController(MapstatsContext context)
         {
-            return View();
+            _context = context;
         }
 
-        public IActionResult Error()
+        // 
+        // GET: /mapstats/
+
+        public IActionResult Index(int time = -1, string timeunit = "days")
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (timeunit == "alltime")
+            {   //Easiest way to do this is just set it to -1 and let everything else handle it
+                time = -1;
+            }
+
+            var validatedTime = time;
+            if (validatedTime < 1)
+            {
+                validatedTime = 10;
+            }
+
+            ViewData["rawtime"] = time;
+            ViewData["time"] = validatedTime;
+            ViewData["timeunit"] = timeunit;
+
+            var model = new MapstatsViewModel();
+            var serverListRaw = _context.MapstatsServers.ToList().OrderBy(x => x.Engine);
+
+            IList<ServerViewModel> serverList = new List<ServerViewModel>();
+
+            Dictionary<int, IEnumerable<MapDataViewModel>> dataList = new Dictionary<int, IEnumerable<MapDataViewModel>>();
+            
+            foreach (var server in serverListRaw)
+            {
+                if (server.Engine == "")
+                {
+                    server.Engine = "other";
+                }
+                ServerViewModel serverViewModel = new ServerViewModel
+                {
+                    ServerId = server.ServerId,
+                    Ip = server.Ip,
+                    ServerName = server.ServerName,
+                    Version = server.Version,
+                    EngineImg = "/images/games/" + server.Engine + ".png"
+                };
+
+                var timeSince = System.DateTimeOffset.Now;
+                if (timeunit == "days")
+                {
+                    timeSince = timeSince.AddDays(0 - time);
+                }
+                else if (timeunit == "months")
+                {
+                    timeSince = timeSince.AddMonths(0 - time);
+                }
+                
+                if (time < 1) //If time is invalid, assume from all time
+                {
+                    timeSince = System.DateTimeOffset.MinValue;
+                }
+
+                serverList.Add(serverViewModel);
+                var dataQuery = from maps in _context.MapstatsMaps
+                                join data in _context.MapstatsData.Where(x => x.Timestamp > timeSince)
+                                on maps.MapId equals data.MapId into groupedData
+                                where maps.ServerId == server.ServerId
+                                select new MapDataViewModel
+                                {
+                                    ServerId = maps.ServerId,
+                                    MapName = maps.MapName,
+                                    Connects = maps.Connects,
+                                    Disconnects = maps.Disconnects,
+                                    Ratio = ((float)maps.Connects) / ((float)maps.Disconnects),
+                                    PlayerHours = ((float)groupedData.Sum(x => x.PlayerCount * x.DataInterval)) / 60.0f,
+                                    ServerHours = ((float)groupedData.Sum(x => x.DataInterval)) / 60.0f,
+                                    DataPoints = groupedData.Count()
+                                };
+                dataQuery = dataQuery.OrderByDescending(x => x.Ratio);
+                dataList[server.ServerId] = dataQuery.ToList();
+            }
+
+            model.Servers = serverList;
+            model.Data = dataList;
+            return View(model);
         }
     }
 }
